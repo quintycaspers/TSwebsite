c5d0b5f4a824d0a1086fcfbd0de6fb00eb41cfc7
quintycaspers <quintycaspers@gmail.com>
2023-12-06 10:29:19 +0100

    Added basics and images

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
-// JavaScript Document
-console.log("hi");
+/****************/
+// hamburger menu 
+/****************/
+
+const hamburgerButton = document.querySelector("nav button"); 
+const navList = document.querySelector("nav ul");
+
+hamburgerButton.addEventListener("click", toggleMenu);
+function toggleMenu() {
+    if (navList.classList.contains("menu-closed")) {
+        console.log("open");
+        navList.classList.add("menu-open");
+        navList.classList.remove("menu-closed");
+    }
+
+    else if (navList.classList.contains("menu-open")) {
+        console.log("closed");
+        navList.classList.add("menu-closed");
+        navList.classList.remove("menu-open");
+    }
+}
+
